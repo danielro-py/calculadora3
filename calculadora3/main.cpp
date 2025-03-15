150fd0de1c83cd5727fd3551e12c3b525b34df0b
danielro <daniel.rojas@alumnos.uiwbajio.mx>
2025-03-15 14:26:47 -0600

    Initial Commit

---
+++ calculadora3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calculadora3
+//
+//  Created by Daniel on 15/03/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
